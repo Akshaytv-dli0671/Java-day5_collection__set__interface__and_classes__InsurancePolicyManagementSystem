399fecbbc0d3f4c9fc2d11b18ec802f7f4ef1a53
Akshay TV <Akshay.TV@godigit.com>
2024-07-29 17:21:18 +0530

    First non duplicate element using linkedhashset_updated

--- Collection_set/First_nonDuplicate.java
+++ Collection_set/First_nonDuplicate.java
@@ -8,26 +8,30 @@ public class First_nonDuplicate {
     public static void main(String[] args) {
         List<Integer>list=new ArrayList<>();
         list.add(4);
-        list.add(5);
-        list.add(6);
         list.add(4);
+        list.add(4);
+        list.add(6);
         list.add(7);
         list.add(8);
         list.add(5);
-        LinkedHashSet set=new LinkedHashSet();
+        LinkedHashSet<Integer> set=new LinkedHashSet();
+        LinkedHashSet<Integer>duplicate=new LinkedHashSet<>();
+
         for(int i:list){
             if(set.contains(i)){
-                set.remove(i);
+                duplicate.add(i);
             }
             else{
                 set.add(i);
             }
         }
-        for(Object i: set){
-            System.out.println(i);
-            return;
+        for(Integer i: set){
+            if(duplicate.contains(i)==false){
+                System.out.println(i);
+                return;
+            }
 
         }
-        
+
     }
 }
