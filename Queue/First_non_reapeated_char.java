91c5437c14d62f56a6c226a5c739d45b55d7f3d7
Akshay TV <Akshay.TV@godigit.com>
2024-08-01 09:51:33 +0530

    First no repeated character

---
+++ Queue/First_non_reapeated_char.java
@@ -0,0 +1,33 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class First_non_reapeated_char {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String string=scanner.next();
+        Queue<Character> queue=new LinkedList<>();
+        char[] characters=string.toCharArray();
+        int[] frequency=new int[26];
+        for(char character:characters)
+        {
+            queue.add(character);
+            frequency[character-'a']++;
+        }
+        boolean change=false;
+        while(!queue.isEmpty())
+        {
+            if(frequency[queue.peek()-'a']>1)
+            {
+                queue.remove();
+            }
+            else
+                break;
+        }
+
+        System.out.println(queue.peek());
+    }
+}
